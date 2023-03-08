bd98e13cf2ab142f2174fed9b84703eae66ec951
SMagar123 <surajpulamim123@gmail.com>
2023-03-08 10:10:36 +0545

    copyright and coins height updated

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -4,7 +4,7 @@ const Copyright = () => {
   return (
     <div style={{ marginTop: "1em" }}>
       <h4 style={{ color: "white" }}>Copyright @ MeroCoins 2023</h4>
-      <h5 style={{ color: "white" }}>Practiced by ULLU</h5>
+      <h5 style={{ color: "white" }}>Practiced by Suraj Pulami Magar</h5>
     </div>
   );
 };
